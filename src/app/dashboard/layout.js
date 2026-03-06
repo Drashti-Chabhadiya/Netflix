faa35f1c96bdda1ce1a843521d4cbbb19e931751
Drashti-Chabhadiya <chabhadiyadrashti@gmail.com>
2026-03-06 20:21:08 +0530

    feat: Add protected layout for dashboard routes.

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,5 +1,6 @@
 import ProtectedLayout from '../components/ProtectedLayout';
 
+
 export default function DashboardLayout({ children }) {
   return <ProtectedLayout redirectTo="/">{children}</ProtectedLayout>;
 }
